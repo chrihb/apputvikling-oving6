d44869d0f5613f634b167b5494b811d78ff6e814
chrihb <chrihb@stud.ntnu.no>
2025-10-01 19:32:44 +0200

    done

--- app/src/main/java/ntnu/idi/oving6/ChatClient.kt
+++ app/src/main/java/ntnu/idi/oving6/ChatClient.kt
@@ -1,4 +1,33 @@
 package ntnu.idi.oving6
 
-class ChatClient {
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
+
+class ChatClient(private val HOST: String, private val PORT: Int, private val onMessage: (String) -> Unit) {
+    private lateinit var socket: Socket
+
+    fun start() {
+        CoroutineScope(Dispatchers.IO).launch {
+            socket = Socket(HOST, PORT)
+            Log.d("ChatClient", "Connected to $HOST:$PORT" )
+            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            while (true) {
+                val line = reader.readLine() ?: break
+                Log.d("ChatClient", "From server $line" )
+                onMessage(line)
+            }
+        }
+    }
+
+    fun send(deviceId: String, message: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            PrintWriter(socket.getOutputStream(), true).println("$deviceId: $message")
+        }
+    }
 }
--- app/src/main/java/ntnu/idi/oving6/MainActivity.kt
+++ app/src/main/java/ntnu/idi/oving6/MainActivity.kt
@@ -1,13 +1,63 @@
 package ntnu.idi.oving6
 
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.security.MessageDigest
 
 
 class MainActivity : AppCompatActivity() {
+    private val PORT = 3000
+    private val HOST = "10.0.2.2"
+    private lateinit var client: ChatClient
+    private lateinit var server: ChatServer
+    private lateinit var deviceId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        deviceId = generateTimeHash()
+    }
+
+
+    fun onSendButtonClick(v: View?) {
+        val input = findViewById<EditText>(R.id.messageInput).text.toString()
+        client.send(deviceId, input)
+    }
+
+    fun onStartServerClick(v: View?) {
+        val localhost = "127.0.0.1"
+
+        server = ChatServer(PORT)
+        server.start()
+        client = ChatClient(localhost, PORT) { message ->
+            MainScope().launch {
+                val chatHistory = findViewById<TextView>(R.id.chatHistory)
+                chatHistory.append("\n$message")
+            }
+        }
+        client.start()
+    }
+
+    fun onConnectAsClientClick(v: View?) {
+        client = ChatClient(HOST, PORT) { message ->
+            MainScope().launch {
+                val chatHistory = findViewById<TextView>(R.id.chatHistory)
+                chatHistory.append("\n$message")
+            }
+        }
+        client.start()
+    }
+
+    fun generateTimeHash(): String {
+        val now = System.currentTimeMillis().toString()
+        val bytes = MessageDigest.getInstance("MD5").digest(now.toByteArray())
+        return bytes.joinToString("") { "%02x".format(it) }.take(6)
     }
 }
